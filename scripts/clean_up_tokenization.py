e08a005ca328b31735cf02d0bdc1d046057387b9
Arthur Mariano Rocha de Azevedo Scalercio <arthurscalercio@gmail.com>
2020-06-18 21:58:24 -0300

    cleaning2
    Signed-off-by: Arthur Mariano Rocha de Azevedo Scalercio <arthurscalercio@gmail.com>

--- scripts/clean_up_tokenization.py
+++ scripts/clean_up_tokenization.py
@@ -6,6 +6,8 @@ def init_args():
   parser = argparse.ArgumentParser(description='Clean up the sequences')
   parser.add_argument('--seed', type=int, default=77777, metavar='S', help='random seed')
   parser.add_argument('--data_path', type=str, default="", help='data path')
+  parser.add_argument("--train", action="store_true", 
+    help="whether to save a train file")
   args = parser.parse_args()
   args.cuda = torch.cuda.is_available()
   return args
@@ -25,9 +27,13 @@ if __name__ == "__main__":
             .replace(" 's", "'s")
             .replace(" 've", "'ve")
             .replace(" 're", "'re")
+            .replace(" :", ":")
+            .replace(" ;", ";")
          for line in f.read().splitlines()]
     
+    train =  args.train
     path, file = os.path.split(args.data_path)
-    with open(path + "/train_cleaned.txt", "w") as f:
+
+    with open(path + "/cleaned_" + file, "w") as f:
         for line in lines:
             f.write(line+"\n")
