bd2b8c5004df229925254b27d6fb9188e17d32f7
Arthur Mariano Rocha de Azevedo Scalercio <arthurscalercio@gmail.com>
2020-12-08 21:49:36 -0200

    results_mass2
    Signed-off-by: Arthur Mariano Rocha de Azevedo Scalercio <arthurscalercio@gmail.com>

--- scripts/log_process.py
+++ scripts/log_process.py
@@ -18,23 +18,39 @@ if __name__ == "__main__":
     args = init_args()
     print(args.data_path)
     lines=[]
-    with open(args.data_path, encoding="utf-8") as f:
-        for line in f.read().splitlines():
-            if line.split(" ")[0]:
-                lines.append(line.split(" ")[-11:])
+    if args.train:
+        with open(args.data_path, encoding="utf-8") as f:
+            for line in f.read().splitlines():
+                if line.split(" ")[0]:
+                    lines.append(line.split(" ")[-22:])
+
+        np_line = np.array(lines)
+        print(np_line.shape)
+        #print(np_line[0])
+        np_line = np.concatenate((np_line[:,0:1], np_line[:, 3:4], np_line[:, 6:7], np_line[:, 9:10], np_line[:, 12:13], np_line[:, 15:16], np_line[:, 18:19], np_line[:, 21:22]), axis = 1)
+        np_float = np_line.astype(np.float)
+        print(np_float)
+    else:
+        with open(args.data_path, encoding="utf-8") as f:
+            for line in f.read().splitlines():
+                if line.split(" ")[0]:
+                    lines.append(line.split(" ")[-11:])
                 #print(len(line.split(" ")[-11:]))
                 #print(line.split(" ")[-11:])
 
-    np_line = np.array(lines)
-    print(np_line.shape)
-    #print(np_line.size)
-    ##print(np_line[:,3])
-    np_line = np.concatenate((np_line[:,0:1], np_line[:, 2:3], np_line[:, 4:5], np_line[:, 6:7]), axis = 1)
-    np_float = np_line.astype(np.float)
-    print(np_float)
-    train =  args.train
-    path, file = os.path.split(args.data_path)
-    np.savetxt("Nov25030636" + "log.csv" ,np_float, delimiter = ",")
+        np_line = np.array(lines)
+        print(np_line.shape)
+        np_line = np.concatenate((np_line[:,0:1], np_line[:, 2:3], np_line[:, 4:5], np_line[:, 6:7]), axis = 1)
+        np_float = np_line.astype(np.float)
+        print(np_float)
+        #train =  args.train
+        path, file = os.path.split(args.data_path)
+
+    if args.train:
+        np.savetxt("Nov24215804" + "train_log.csv" ,np_float, delimiter = ",")
+
+    else:
+        np.savetxt("Nov25030636" + "log.csv" ,np_float, delimiter = ",")
 
     
     
